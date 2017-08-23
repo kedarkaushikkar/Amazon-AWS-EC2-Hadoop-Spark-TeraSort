90823b6de38c09335bdac1ca7dd2fe6e5087edcd
Kedar Kaushikkar <kedarkaushikkar19@gmail.com>
2017-08-22 20:09:40 -0500

    change

--- Source Files/spark_sort.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkContext
-
-sc=SparkContext(appName="Sort")
-file_data=sc.textFile("/usr/sparkusr/input/input.txt")
-key_value_data=file_data.map(lambda x: (str(x[0:10]), str(x[10:100])))
-sorted_data=key_value_data.sortByKey(True)
-output_data=sorted_data.map(lambda x :(''.join(str(a) for a in x)))
-output_data.saveAsTextFile("/usr/sparkusr/output/")
